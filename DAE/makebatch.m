cd386953224b402b24dfe1b57349abd5cf2ee1f5
renbo <justnow.ren@gmail>
2014-06-02 14:26:47 +0900

    remove no need log because of performance

--- DAE/makebatch.m
+++ DAE/makebatch.m
@@ -12,12 +12,12 @@ function makebatch()
     sample_map = {};
     index = 0;
     sum = 0;
+    disp('############ Map File Begin ##############');
     while 1
         filename = fgetl(fid);
         if ~ischar(filename)
             break;
         end
-        disp(sprintf('Map File :%s\n',filename));
         cap = read_num(filename);
         index = index+1;
         sum = sum+cap;
@@ -26,22 +26,26 @@ function makebatch()
         sample_map{index,3} = cap;
     end
     fclose(fid);
+    disp('############ Map File End ##############');
     %% make the batch map
     rand('state',0);
     randlist = randperm(sum);
     sample_list = {};
-    
+    disp('############ Build Batch Begin ##############');
     for n = 1:length(randlist)
+        %disp(sprintf('Batch No %d', n));
         for idx = 1:length(sample_map)
             num = randlist(n);
             if num <= sample_map{idx,1} 
                 sample_list{n,1}=sample_map{idx,2}; %the features file
                 sample_list{n,2}=sample_map{idx,3} + num - sample_map{idx,1}; % offset in
-                disp(sprintf('Batch No %d  Offset : %d ,File: %s\n', num,sample_list{n,2},sample_list{n,1}));
+                                                                              %        disp(sprintf('Batch No %d  Offset : %d ,File: %s\n', num,sample_list{n,2},sample_list{n,1}));
                 break;
             end
         end
     end
+    disp(sprintf('Builded Batch %d'),size(sample_list,1));
+    disp('############ Build Batch End ##############');
     save(sample_list, 'sample_list');
 end
 
