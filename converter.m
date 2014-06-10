8cb435686cfa0425fd57d4be4ecf14599afad7c8
renbo <justnow.ren@gmail>
2014-06-11 01:29:08 +0900

    update the converter with some modifies

--- converter.m
+++ converter.m
@@ -27,7 +27,7 @@
 function converter()
     INI = ini2struct('conf.ini');
     % get file list
-    list = filelist(INI.input.testlist, INI.input.feature_dir);
+    list = filelist(INI.in.testlist, INI.in.feature_dir);
     if length(list)==0
         disp_err('List file', '');
     end
@@ -37,7 +37,7 @@ function converter()
     [ave, var] = mean_var(samples);
     %fprintf('Mean : %s , Var : %s \n', ave, var);
     % normalization 
-    normalization(samples, ave, var);
+    normalization(samples, ave, var, INI.in.data_dir);
 end
 
 function [ave, var] = mean_var(data)
@@ -50,69 +50,64 @@ function [ave, var] = mean_var(data)
     var=std(total,0,2);
 end
 
-function normalization(data, ave, var)
+function normalization(data, ave, var, store_dir)
     disp('begin to normalization');
     num_files = length(data);
+    if (nargin < 4)
+        store_dir = '';
+    end
     %TODO make this more clear
     for idx = 1:num_files
         rawdata=bsxfun(@rdivide,bsxfun(@minus,data{idx}.data,ave),var);
         rawdata=1./(1+exp(-rawdata));
         D = [];
-        num_smples = size(data{idx}.data,2);
-        for smp_idx = 1:num_smples-8
+        ext = data{idx}.ext;
+        for smp_idx = 1:ext.nSamples-8
             %% fprintf(asci_wf,'%f ',rawdata([1:39],a:a+8))
             joint_smaples = rawdata(1:39,smp_idx:smp_idx+8);
             feature = joint_smaples(:);
             D = [D;feature'];
         end
         fprintf('%5d Digits of class %d_%d\n',size(D,1),idx,num_files);
-        save([data{idx}.file,'.mat'],'D','-mat');
+
+        if (~isempty(store_dir))
+            outfile = strcat(store_dir, data{idx}.part, '.mat');
+            check_basedir(outfile);
+            fprintf('Save Data to %s\n', outfile);
+            save(outfile,'D', 'ext', '-mat');
+        end
         clear('D');
     end
-end
-
-function list = filelist(file, basedir)
-    list = {};
-    fid = fopen(file);
-    if (fid == -1)
-        disp(fprintf('[Error] [Read FileList] %s', file));
-        return;
-    end
-    items = textscan(fid, '%s', 'delimiter', '\n');
-    subitems = items{1};
-    
-    for idx = 1:length(subitems)
-        list{end+1} = strcat(basedir, '/', subitems{idx});
-        %fprintf('%s\n', list{idx});
-    end
-    fclose(fid);
-end
-
-function disp_err(msg, ext)
-    type = 1;
-    msg_type = {'Warning', 'Dead'};
-    disp(sprintf('[%s] [%s] [%s]', msg_type{type}, msg, ext))
 end
 
 function samples = data (list)
     samples = {};
     for idx = 1:length(list)
-        fid =fopen(list{idx},'r');
+        fid =fopen(list{idx}.fullpath,'r');
         if (fid == -1)
-            disp_err('ReadSample', list{idx});
+            disp_err('ReadSample', list{idx}.fullpath);
             continue;
         end
-        nSamples = fread(fid,1,'int','b');
-        sampPeriod = fread(fid,1,'int','b');
-        sampSize = fread(fid,1,'short','b');
-        parmKind = fread(fid,1,'short','b');
-        rawdata = fread(fid,nSamples*(sampSize/4),'float','b');
-        rawdata = reshape(rawdata,sampSize/4,nSamples);
-        samples{end+1} = struct('file',list{idx}, 'data', rawdata);
+        ext.nSamples = fread(fid,1,'int','b');
+        ext.sampPeriod = fread(fid,1,'int','b');
+        ext.sampSize = fread(fid,1,'short','b');
+        ext.parmKind = fread(fid,1,'short','b');
+        rawdata = fread(fid,ext.nSamples*(ext.sampSize/4),'float','b');
+        rawdata = reshape(rawdata,ext.sampSize/4,ext.nSamples);
+        
+        samples{idx}.part = list{idx}.part;
+        samples{idx}.ext = ext;
+        samples{idx}.data = rawdata;
         fclose(fid);
     end
     if (length(list) ~= length(samples))
         disp_err('Listed file missed!','');
     end
 end
+
+
+
+
+
+
 
